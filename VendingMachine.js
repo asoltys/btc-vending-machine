905d6362632309cb1535c9e433484d237f7cae31
any <anythingispopsicle@gmail.com>
2016-06-13 22:09:34 -0700

    Keep track of transactions.

--- VendingMachine.js
+++ VendingMachine.js
@@ -7,6 +7,8 @@ const utxoPollPromise = require('./blockchaininfo');
 const products = require('./products');
 const addressMap = {};
 const exec = require('child_process').exec;
+const txs = [];
+
 products.forEach(product=>{
   addressMap[product.address] = {
     price: product.price,
@@ -14,9 +16,27 @@ products.forEach(product=>{
   }
 })
 
+// Create a random transaction for testing:
+function randomTx() {
+    return {
+        txid: Math.round(Math.random()*3).toString(),
+        recieved: 0.0003,
+        address: products[0].address }
+}
+//const dummy = Kefir.fromPoll(1000, randomTx)
+
 Promise.all([webSocketPromise, utxoPollPromise])
   .then(paymentStreamArray=>{
-    const allPaymentStreams = Kefir.merge(paymentStreamArray).log('merged payment layer triggered')
+    const allPaymentStreams = Kefir
+        .merge(paymentStreamArray)
+        .log()
+        .filter(payment => { 
+            if (txs.indexOf(payment.txid) != -1) {
+                return false 
+            }
+            txs.push(payment.txid)
+            return true
+        })
     const normalizedPaymentStream = Kefir.combine([allPaymentStreams,exchangeRateStream], (payment, exchangeRate)=>{
         var paymentCents = payment.recieved * exchangeRate * 100;
         var normalizedPayment = paymentCents / addressMap[payment.address].price;
